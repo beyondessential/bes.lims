7ceec70e12dbc8e3fcfb785649f87501bc441188
Jordi Puiggené <jp@naralabs.com>
2024-09-08 07:01:02 +0200

    Add Reagent "Out of Stock" and "Rollback" Analysis transitions

--- src/bes/lims/setuphandlers.py
+++ src/bes/lims/setuphandlers.py
@@ -5,10 +5,12 @@
 # Copyright 2024 Beyond Essential Systems Pty Ltd
 
 from bes.lims import logger
+from bes.lims import permissions
 from senaite.core.setuphandlers import setup_core_catalogs
 from senaite.core.setuphandlers import setup_other_catalogs
 from senaite.core.catalog import ANALYSIS_CATALOG
-from senaite.core.catalog import SAMPLE_CATALOG
+from senaite.core.workflow import ANALYSIS_WORKFLOW
+from senaite.core.api import workflow as wapi
 
 
 CATALOGS = (
@@ -23,6 +25,50 @@ INDEXES = [
 COLUMNS = [
     # Tuples of (catalog, column_name)
 ]
+
+# Workflow updates
+WORKFLOWS_TO_UPDATE = {
+    ANALYSIS_WORKFLOW: {
+        "states": {
+            "assigned": {
+                "transitions": ["set_out_of_stock"],
+            },
+            "unassigned": {
+                "transitions": ["set_out_of_stock"],
+            },
+            "out_of_stock": {
+                "title": "Out of Stock",
+                "transitions": ["rollback"],
+                "permissions_copy_from": "rejected",
+            },
+        },
+        "transitions": {
+            "set_out_of_stock": {
+                "title": "Set Out of Stock",
+                "new_state": "out_of_stock",
+                "action": "Set Out of Stock",
+                "after_script": "",
+                "guard": {
+                    "guard_permissions": permissions.TransitionSetOutOfStock,
+                    "guard_roles": "",
+                    "guard_expr":
+                        "python:here.guard_handler('set_out_of_stock')",
+                }
+            },
+            "rollback": {
+                "title": "Rollback",
+                "new_state": "",
+                "action": "Rollback to previous status",
+                "after_script": "",
+                "guard": {
+                    "guard_permissions": permissions.TransitionRollback,
+                    "guard_roles": "",
+                    "guard_expr": "python:here.guard_handler('rollback')",
+                }
+            },
+        }
+    },
+}
 
 
 def setup_handler(context):
@@ -39,6 +85,9 @@ def setup_handler(context):
     # Setup Catalogs
     setup_catalogs(portal)
 
+    # Setup workflows
+    setup_workflows(portal)
+
     logger.info("BES setup handler [DONE]")
 
 
@@ -51,3 +100,12 @@ def setup_catalogs(portal):
     setup_other_catalogs(portal, indexes=INDEXES, columns=COLUMNS)
 
     logger.info("Setup Catalogs [DONE]")
+
+
+def setup_workflows(portal):
+    """Setup workflow changes (status, transitions, permissions, etc.)
+    """
+    logger.info("Setup workflows ...")
+    for wf_id, settings in WORKFLOWS_TO_UPDATE.items():
+        wapi.update_workflow(wf_id, **settings)
+    logger.info("Setup workflows [DONE]")
---
+++ src/bes/lims/upgrade/v01_00_000.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+
+from bes.lims import logger
+from bes.lims import PRODUCT_NAME as product
+from bes.lims.setuphandlers import setup_workflows
+from bika.lims import api
+from senaite.core.catalog import ANALYSIS_CATALOG
+from senaite.core.upgrade import upgradestep
+from senaite.core.upgrade.utils import UpgradeUtils
+from senaite.core.workflow import ANALYSIS_WORKFLOW
+
+version = "1.0.0"  # Remember version number in metadata.xml and setup.py
+profile = "profile-{0}:default".format(product)
+
+
+@upgradestep(product, version)
+def upgrade(tool):
+    portal = tool.aq_inner.aq_parent
+    ut = UpgradeUtils(portal)
+    ver_from = ut.getInstalledVersion(product)
+
+    if ut.isOlderVersion(product, version):
+        logger.info("Skipping upgrade of {0}: {1} > {2}".format(
+            product, ver_from, version))
+        return True
+
+    logger.info("Upgrading {0}: {1} -> {2}".format(product, ver_from, version))
+
+    # -------- ADD YOUR STUFF BELOW --------
+
+    logger.info("{0} upgraded to version {1}".format(product, version))
+    return True
+
+
+def setup_analysis_workflow(tool):
+    """Adds the analysis workflow portal action
+    and updates the analyses states and transitions
+    """
+    logger.info("Setup analysis workflow ...")
+
+    #  Update Analyses workflow
+    portal = tool.aq_inner.aq_parent
+    setup = portal.portal_setup
+    setup.runImportStepFromProfile(profile, "rolemap")
+    setup_workflows(api.get_portal())
+
+    # Update Analyses rolemap
+    statuses = ["assigned", "unassigned"]
+    query = {"portal_type": "Analysis", "review_state": statuses}
+    brains = api.search(query, ANALYSIS_CATALOG)
+
+    wf_tool = api.get_tool("portal_workflow")
+    wf = wf_tool.getWorkflowById(ANALYSIS_WORKFLOW)
+
+    for brain in brains:
+        obj = api.get_object(brain)
+        wf.updateRoleMappingsFor(obj)
+
+        # Flush the object from memory
+        obj._p_deactivate()
+
+    logger.info("Setup analysis workflow [DONE]")
