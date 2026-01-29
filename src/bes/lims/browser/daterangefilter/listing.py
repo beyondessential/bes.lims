d7aa6087e30a59f9595486888df113feed3a6180
nnpvaan <84114449+nnpvaan@users.noreply.github.com>
2026-01-29 15:53:23 +0700

    Fix date range filter timezone handling (#107)
    * Fix date range filter timezone handling
    
    * Do not convert to timezone, but replace
    
    ---------
    
    Co-authored-by: Jordi Puiggené <jp@naralabs.com>

--- src/bes/lims/browser/daterangefilter/listing.py
+++ src/bes/lims/browser/daterangefilter/listing.py
@@ -18,13 +18,35 @@
 # Copyright 2024-2025 by it's authors.
 # Some rights reserved, see README and LICENSE.
 
+import pytz
 from bes.lims.browser.daterangefilter import get_selected_date_range_config
-from senaite.core.api import dtime
+from bika.lims import api
+from plone.memoize import view
 from senaite.app.listing.interfaces import IListingView
 from senaite.app.listing.interfaces import IListingViewAdapter
+from senaite.core.api import dtime
 from zope.component import adapter
 from zope.interface import implementer
-from plone.memoize import view
+
+
+def get_timezone():
+    """Return the effective timezone for the current user.
+    Prefer the user's timezone if set and valid, otherwise fall back to the
+    site timezone, and finally to the system (OS) timezone.
+    """
+    # User's default TZ has priority over portal's
+    user = api.get_current_user()
+    tz = user.getProperty("timezone")
+    if dtime.is_valid_timezone(tz):
+        return tz
+
+    # get the portal's default timezone
+    tz = api.get_registry_record("plone.portal_timezone")
+    if dtime.is_valid_timezone(tz):
+        return tz
+
+    # No TZ set or not valid, return OS's
+    return dtime.get_os_timezone()
 
 
 @adapter(IListingView)
@@ -63,19 +85,30 @@ class DateRangeFilterListingAdapter(object):
         if not date_range.get("filter_enabled", False):
             return {}
 
-        datetime_from = date_range.get("datetime_from")
-        datetime_to = date_range.get("datetime_to")
+        # get the date range and index name to search by (date_type)
+        dt_from = dtime.to_dt(date_range.get("datetime_from"))
+        dt_to = dtime.to_dt(date_range.get("datetime_to"))
         date_type = date_range.get("date_type")
-        if not datetime_from and not datetime_to:
+        if not all([dt_from, dt_to, date_type]):
             return {}
 
-        date_from_dt = dtime.to_DT(datetime_from)
-        date_to_dt = dtime.to_DT(datetime_to)
+        # Dates submitted with the form are timezone-naive. We therefore
+        # explicitly set (without shifting the time value) the current
+        # user/site/system timezone so that searches align with the timezone
+        # used during indexing.
+        timezone = get_timezone()
+        tz_info = pytz.timezone(timezone)
+        dt_from = dt_from.replace(tzinfo=tz_info)
+        dt_to = dt_to.replace(tzinfo=tz_info)
+
+        # convert to Zope's DateTime
+        dt_from = dtime.to_DT(dt_from)
+        dt_to = dtime.to_DT(dt_to)
 
         # build the query - filter by the selected date type
         query = {
             date_type: {
-                "query": [date_from_dt, date_to_dt],
+                "query": [dt_from, dt_to],
                 "range": "min:max"
             }
         }
