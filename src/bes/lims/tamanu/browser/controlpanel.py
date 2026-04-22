941610ba52b8f883bdb98bed6c1f1cee834d4527
Jordi Puiggené <jp@naralabs.com>
2026-04-22 16:01:33 +0200

    Set a password placeholder

--- src/bes/lims/tamanu/browser/controlpanel.py
+++ src/bes/lims/tamanu/browser/controlpanel.py
@@ -3,9 +3,13 @@
 from bes.lims.tamanu import _
 from plone.app.registry.browser.controlpanel import ControlPanelFormWrapper
 from plone.app.registry.browser.controlpanel import RegistryEditForm
+from plone.registry.interfaces import IRegistry
 from plone.supermodel import model
 from plone.z3cform import layout
 from zope import schema
+from zope.component import getUtility
+
+PASSWORD_PLACEHOLDER = u"•" * 12
 
 
 class ITamanuControlPanel(model.Schema):
@@ -73,6 +77,28 @@ class TamanuControlPanelForm(RegistryEditForm):
         u"controlpanel_tamanu",
         default=u"Tamanu Synchronization Settings"
     )
+
+    def updateWidgets(self):
+        super(TamanuControlPanelForm, self).updateWidgets()
+        # The password widget's template intentionally never emits a `value`
+        # attribute so the secret is not round-tripped to the browser. That
+        # makes a configured password look identical to an unconfigured one.
+        # Render dots as a placeholder when a password is stored, so admins
+        # can tell at a glance that a value is set.
+        pw_widget = self.widgets.get("password")
+        if pw_widget is None:
+            return
+        registry = getUtility(IRegistry)
+        if registry.get(self.schema_prefix + ".password"):
+            pw_widget.placeholder = PASSWORD_PLACEHOLDER
+
+    def applyChanges(self, data):
+        # Treat an empty password submission as "do not change", so that
+        # re-saving the form without retyping the password preserves the
+        # stored value. To clear the password, unset it via the registry.
+        if not data.get("password"):
+            data.pop("password", None)
+        return super(TamanuControlPanelForm, self).applyChanges(data)
 
 
 TamanuControlPanelView = layout.wrap_form(
