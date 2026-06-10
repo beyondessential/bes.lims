0921934e01bea394b3091e6aabf9d0ce593fc351
nnpvaan <phuongvan0609@gmail.com>
2026-06-10 15:57:07 +1000

    Disabled notification for sample status transition to 'to_be_verified'

--- src/bes/lims/tamanu/config.py
+++ src/bes/lims/tamanu/config.py
@@ -59,7 +59,8 @@ SEND_OBSERVATIONS = True
 SAMPLE_STATUSES = (
     # mapping between sample status and tamanu statuses
     ("sample_received", "partial"),
-    ("to_be_verified", "preliminary"),
+    # Do not notify Tamanu when sample transitions to 'to_be_verified'
+    # ("to_be_verified", "preliminary"),
     ("published", "final"),
     ("invalid", "entered-in-error"),
     # Tamanu is notified back with the rejection report (Attachment) instead of
