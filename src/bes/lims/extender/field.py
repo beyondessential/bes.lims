fabeb44b0a90ac08412e311665b7e54beef1368e
Jordi Puiggené <jp@naralabs.com>
2026-07-20 12:25:22 +0200

    Allow custom text for SamplePoint (Site) field (#173)
    * Allow custom text for SamplePoint (Site) field
    
    * Changelog

--- src/bes/lims/extender/field.py
+++ src/bes/lims/extender/field.py
@@ -19,12 +19,14 @@
 # Some rights reserved, see README and LICENSE.
 
 from archetypes.schemaextender.field import ExtensionField as ATExtensionField
+from bika.lims import api
 from bika.lims.browser.fields import UIDReferenceField
 from plone.app.blob.field import ImageField as BlobImageField
 from Products.Archetypes.Field import FixedPointField
 from Products.Archetypes.Field import StringField
 from Products.Archetypes.Field import BooleanField
 from Products.Archetypes.Field import IntegerField
+from Products.Archetypes.Field import LinesField
 from Products.Archetypes.Field import TextField
 from senaite.core.browser.fields.datetime import DateTimeField
 from senaite.core.browser.fields.records import RecordsField
@@ -82,3 +84,36 @@ class ExtTextField(ExtensionField, TextField):
 class ExtUIDReferenceField(ExtensionField, UIDReferenceField):
     """Field Extender of core's UIDReferenceField for AT types
     """
+
+
+class ExtSiteField(ExtensionField, LinesField):
+    """Extended Field for Site, that allows both the selection of a SamplePoint
+    object and the introduction of a custom text value. The value is proxied to
+    the SamplePoint field so index searches keep working as expected
+    """
+
+    def get(self, instance, **kw):
+        return getattr(instance, "_sample_point", None)
+
+    def set(self, instance, value, **kw):
+        if api.is_string(value):
+            value = filter(None, value.split("\r\n"))
+
+        if not isinstance(value, (tuple, list, set)):
+            value = tuple((value, ))
+        out = set()
+        # reset sample point
+        sample_point = instance.getField("SamplePoint")
+        sample_point.set(instance, None)
+        for val in value:
+            if api.is_uid(val):
+                val = api.get_object_by_uid(val, default=None)
+            if api.is_object(val):
+                obj = api.get_object(val)
+                val = api.get_title(obj)
+                # proxy to sample point
+                uid = api.get_uid(obj)
+                sample_point.set(instance, uid)
+            if val and api.is_string(val):
+                out.add(val)
+        setattr(instance, "_sample_point", tuple(out))
